fa8240beffc8f3b55bc69ca04d9ade0bb9c5bf8c
lmgutierrez <87157961+lmgutierrez@users.noreply.github.com>
2021-07-24 17:25:39 -0500

    Add files via upload

---
+++ API/api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from flask import Flask, request
+from m09_model_deployment import predict_proba
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['GET'])
+def URLpredict():
+    return {
+         "result": predict_proba(request.args.get('URL'))
+        }, 200
+
+
+@app.route('/hola', methods=['GET'])
+def hola():
+    return {
+         "result": "todo bien"
+        }, 200
+
+
+@app.route('/nombre', methods=['GET'])
+def nombre():
+    return {
+         "nombre": request.args.get('NOMBRE'),
+         "apellido": request.args.get('APELLIDO')
+        }, 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
+
