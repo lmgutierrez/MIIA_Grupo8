b463193fb5c273eabb7890d896fc19302c5ee6de
lbautistam <87158049+lbautistam@users.noreply.github.com>
2021-07-27 16:39:09 -0500

    Add files via upload

--- P2_API_v3/api.py
+++ P2_API_v3/api.py
@@ -19,4 +19,4 @@ def nombre():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8889)
