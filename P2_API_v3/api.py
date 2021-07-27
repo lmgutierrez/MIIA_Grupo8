b1e9b645e57d877402e058bc2b3959348f58bc31
lbautistam <87158049+lbautistam@users.noreply.github.com>
2021-07-27 16:15:32 -0500

    Add files via upload

---
+++ P2_API_v3/api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from flask import Flask, request
+from model_deployment import predict_proba
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['GET'])
+def nombre():
+    return {
+         "Year": request.args.get('Year'),
+         "Mileage": request.args.get('Mileage'),
+         "State": request.args.get('State'),
+         "Make": request.args.get('Make'),
+         "Model": request.args.get('Model'),
+         "result": predict_proba('Year','Mileage','State','Make','Model')
+        }, 200
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=8888)
